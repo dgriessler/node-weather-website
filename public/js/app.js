43fa9bf8570bf9928b1547e8c12793781e055e42
dgriessler <32967199+dgriessler@users.noreply.github.com>
2019-10-04 17:51:12 -0400

    App for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ const search = document.querySelector('input')
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
     const location = search.value
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error)
